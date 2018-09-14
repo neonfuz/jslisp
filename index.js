09dffbe9bac8e1e928a2b05b09b52bb0e0fa0480
Sage Raflik <neonfuz@gmail.com>
2018-09-14 01:53:30 -0700

    Overhaul, fixed many things, lambdas work now

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const scopes = {
   io: {
-    print: message => console.log(message),
+    print: (...messages) => console.log(...messages),
   },
   sym: {
     identity: a => a,
@@ -11,14 +11,24 @@ const scopes = {
     length: list => list.length,
   },
   math: {
-    '+': nums => nums.reduce((a,b) => a+b, 0),
-    '-': nums => nums.reduce((a,b) => a-b, 0),
-    '*': nums => nums.reduce((a,b) => a*b, 1),
-    '/': nums => nums.reduce((a,b) => a+b, 0), // TODO: Make sure order of this is correct lol
+    '+': (...nums) => nums.reduce((a,b) => a+b, 0),
+    '-': (...nums) => nums.reduce((a,b) => a-b, 0),
+    '*': (...nums) => nums.reduce((a,b) => a*b, 1),
+    '/': (...nums) => nums.reduce((a,b) => a/b),
   },
 };
 
-const stdlib = Object.keys(scopes).reduce((acc, key) => ({...acc, ...scopes[key]}), {});
+const constants = {
+  quote: Symbol('quote'), lambda: Symbol('lambda'), apply: Symbol('apply'),
+};
+
+// Flattens out one level of an object
+const objMerge = obj => Object
+  .keys(obj)
+  .map(key => obj[key])
+  .reduce((a, b) => ({...a, ...b}), {});
+
+const stdlib = { ...objMerge(scopes), ...constants };
 
 const q = sym => ['quote', sym];
 const objZip = (keys, vals) => keys.reduce((acc,key,i)=>({...acc, [key]: vals[i]}), {});
@@ -33,20 +43,33 @@ const getFunc = (sym, scope) => {
 }
 
 const exec = (prog, scope) => {
-  const { car, cdr } = stdlib;
-  const sym = car(prog);
-  const args = cdr(prog);
-  switch (sym) {
-    case 'quote': return car(args);
-    case 'lambda': return parseLambda(args, scope);
-    case 'apply': return exec([...getFunc(args[0], scope), ...exec(args[1], scope)], scope);
-    default: 
-      ; const func = getFunc(sym, scope);
-      if (typeof func === 'function')
-        return getFunc(sym, scope).apply(null, args.map(arg => exec(arg, scope)));
-      return func;
+  switch (typeof prog) {
+    case 'number': return prog;
+    case 'string':
+      if (prog in scope) return scope[prog];
+      else throw `Error: Symbol '${prog}' undefined.`;
+    case 'object':
+      if (Array.isArray(prog)) {
+        const [head, ...tail] = prog;
+        const func = exec(head, scope);
+        switch (func) {
+          case constants.quote: return tail[0];
+          case constants.lambda: return parseLambda(tail, scope);
+          case constants.apply:
+            const [ahead, ...atail] = tail;
+            const afunc = exec(ahead, scope);
+            return afunc.apply(null, atail.map(arg => exec(arg, scope)));
+          default:
+            if (typeof func !== 'function')
+              throw `Error: tried executing non function '${JSON.stringify(func)}'`
+            return func.apply(null, tail.map(arg => exec(arg, scope)));
+        }
+      } else {
+        throw `Error: Encountered non array object '${JSON.stringify(prog)}'`;
+      }
   }
-  return prog;
+
+
 };
 
 const add2 = ['lambda', ['a', 'b'], ['+', 'a', 'b']];
@@ -55,7 +78,8 @@ const average = ['lambda', ['list'], ['/', ['apply', '+', 'list'], ['length', 'l
 
 const helloWorld = ['print', q('Hello, world!')];
 
-exec(1, stdlib);
 exec(helloWorld, stdlib);
-exec(['print', ['+', 1, 2]], stdlib);
-//exec(['print', [average, q([1, 2, 3])]], stdlib);
+exec(['print', q('Addition test: '), ['+', 1, 2]], stdlib);
+exec(['print', q('Lambda test: '), [add2, 1, 2]], stdlib);
+exec(['print', ['/', 2, 3]], stdlib);
+exec(['print', [average, q([1, 2, 3])]], stdlib);
